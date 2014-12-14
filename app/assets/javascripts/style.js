017a497fef7c5ba65e37e62c8ffb289afb3c0024
Gaetan Gueraud <gaetan.gueraud@gmail.com>
2014-12-14 15:55:46 +0100

    More swagger

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -1,3 +1,5 @@
+var temp = 0;
+
 function styleChecker() {
     var $styleForm = $('#new_style');
     if (!$styleForm[0]) return;
@@ -9,24 +11,28 @@ function styleChecker() {
         $('#output').empty();
         $('#errorBox').hide();
         $('#loadingBlock').show();
-        progressload();
+        progressLoad();
 
     }).on('ajax:complete', function(xhr, status) {
         var response = status.responseJSON;
         if (status.status == 200) {
             $('#output').html(response.html);
             $('#loadingBlock').hide();
+            $("#tips li").hide();
         }
         else {
             $('#errorBox').show();
             $('#error').text(response.error);
+            $('#loadingBlock').hide();
+            $("#tips li").hide();
         }
     });
 }
 
 $(document).ready(styleChecker);
 $(document).on('page:load', styleChecker);
-function progressload()
+
+function progressLoad()
 {
   var progressBar = $('#loadingBar'),
   width = 0;
@@ -38,9 +44,20 @@ function progressload()
     width += 10;
 
     progressBar.css('width', width + '%');
-
+    showTips();
     if (width >= 100) {
       clearInterval(interval);
     }
   }, 1000)
+}
+
+function showTips()
+{
+  var ran = temp + 1;
+  temp = ran;
+  if ($("#loadingBar").is(":visible"))
+  {
+    $("#tips li").hide();
+    $("#tips li:nth-child(" + ran + ")").fadeIn("fast");
+  }
 }
