d0c41919ebccec7e05832255c2e060ff434aa3e2
Gaetan Gueraud <gaetan.gueraud@gmail.com>
2014-12-14 14:55:06 +0100

    Here comes the progress bar

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -3,14 +3,19 @@ function styleChecker() {
     if (!$styleForm[0]) return;
 
     $('#errorBox').hide();
+    $('#loadingBlock').hide();
 
     $('#new_style').on('submit', function() {
         $('#output').empty();
         $('#errorBox').hide();
+        $('#loadingBlock').show();
+        progressload();
+
     }).on('ajax:complete', function(xhr, status) {
         var response = status.responseJSON;
         if (status.status == 200) {
             $('#output').html(response.html);
+            $('#loadingBlock').hide();
         }
         else {
             $('#errorBox').show();
@@ -21,3 +26,21 @@ function styleChecker() {
 
 $(document).ready(styleChecker);
 $(document).on('page:load', styleChecker);
+function progressload()
+{
+  var progressBar = $('#loadingBar'),
+  width = 0;
+
+  progressBar.width(width);
+
+  var interval = setInterval(function() {
+
+    width += 10;
+
+    progressBar.css('width', width + '%');
+
+    if (width >= 100) {
+      clearInterval(interval);
+    }
+  }, 1000)
+}
